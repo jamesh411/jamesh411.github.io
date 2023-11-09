874f5047c6ba98e1e76617d588ad5cfe6bf7980a
James <jamesh411@berkeley.edu>
2023-11-09 11:15:10 -0800

    organize code

--- js/main.js
+++ js/main.js
@@ -1,96 +1,96 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
   var image;
 
-  $('.popup').click(function(){
-      image = $(this).clone();
+  $('.popup').click(function () {
+    image = $(this).clone();
 
-      iwidth = $(this).width();
-      iheight = $(this).height();
-      wwidth = window.innerWidth;
-      wheight = window.innerHeight;
-      console.log("IWIDTH: " + iwidth + "; IHEIGHT: " + iheight);
-      console.log("WWIDTH: " + wwidth + "; WHEIGHT: " + wheight);
+    iwidth = $(this).width();
+    iheight = $(this).height();
+    wwidth = window.innerWidth;
+    wheight = window.innerHeight;
+    console.log("IWIDTH: " + iwidth + "; IHEIGHT: " + iheight);
+    console.log("WWIDTH: " + wwidth + "; WHEIGHT: " + wheight);
 
-      image.css("display", "none");
+    image.css("display", "none");
 
-      if(wheight < wwidth){
-        console.log('desktop');
-        if (iwidth/iheight > wwidth/wheight) {
-          console.log('image is wider than screen');
-          image.css("width", "90vw");
-          image.css("height", "auto");
-        }
-        else{
-          console.log('image is narrower than screen');
-          image.css("height", "90vh");
-          image.css("width", "auto");
-        }
+    if (wheight < wwidth) {
+      console.log('desktop');
+      if (iwidth / iheight > wwidth / wheight) {
+        console.log('image is wider than screen');
+        image.css("width", "90vw");
+        image.css("height", "auto");
       }
       else {
-        console.log('mobile');
-        if(iwidth/iheight > wwidth/wheight){
-          console.log('image is wider than screen');
-          image.css("width", "90vw");
-          image.css("height", "auto");
-        }
-        else{
-          console.log('image is narrower than screen');
-          image.css("height", "90vh");
-          image.css("width", "auto");
-        }
+        console.log('image is narrower than screen');
+        image.css("height", "90vh");
+        image.css("width", "auto");
       }
+    }
+    else {
+      console.log('mobile');
+      if (iwidth / iheight > wwidth / wheight) {
+        console.log('image is wider than screen');
+        image.css("width", "90vw");
+        image.css("height", "auto");
+      }
+      else {
+        console.log('image is narrower than screen');
+        image.css("height", "90vh");
+        image.css("width", "auto");
+      }
+    }
 
-      image.css({
-        "z-index": "10000",
-        "display": "block",
-        "position": "absolute",
-        "top": "50%",
-        "left": "50%",
-        "transform": "translate(-50%, -50%)"
-      });
-      
-      $("body").append(image);
-      $("#cover").css("display", 'none').fadeIn("slow");
-      image.css("display", 'none').fadeIn("slow");
+    image.css({
+      "z-index": "10000",
+      "display": "block",
+      "position": "absolute",
+      "top": "50%",
+      "left": "50%",
+      "transform": "translate(-50%, -50%)"
+    });
+
+    $("body").append(image);
+    $("#cover").css("display", 'none').fadeIn("slow");
+    image.css("display", 'none').fadeIn("slow");
   });
 
-  $("#cover").click(function(){
+  $("#cover").click(function () {
     $("#cover").css("display", 'block').fadeOut("slow");
     image.css("display", 'block').fadeOut("slow").empty();
   });
 
-  $( function() {
+  $(function () {
     $("#bottle").draggable();
-  } );
-
-  $("#bottle").mousedown(function(){
-    $("#bottle").attr("src","img/spray.png");
   });
 
-  $("#bottle").mouseup(function(){
-    $("#bottle").attr("src","img/bottle.png");
+  $("#bottle").mousedown(function () {
+    $("#bottle").attr("src", "img/spray.png");
   });
 
-  $( function() {
+  $("#bottle").mouseup(function () {
+    $("#bottle").attr("src", "img/bottle.png");
+  });
+
+  $(function () {
     $("#avo").draggable();
-  } );
+  });
 
-  $( function() {
+  $(function () {
     $("#tum").draggable();
-  } );
+  });
 
-  $( function() {
+  $(function () {
     $("#pothos").draggable();
-  } );
+  });
 
-  $(document).ready(function(){
-   if (window.location.hash == "#work") {
-     $('html, body').animate({
+  $(document).ready(function () {
+    if (window.location.hash == "#work") {
+      $('html, body').animate({
         scrollTop: $("#moreInfo").offset().top
       }, 600);
-   }
-});
+    }
+  });
 
 });
 
